88fcc670ada513dd17c1d31fbdc685451b58bfb2
Faryan <118996461+Faryan-Azizian@users.noreply.github.com>
2026-09-15 16:09:48 -0400

    Lab 02 completed

--- lab02.py
+++ lab02.py
@@ -7,27 +7,58 @@
 
 
 def seconds_to_hms(total_seconds):
-    # TODO (Part 1): return the time as a string "H:MM:SS"
-    #   e.g. seconds_to_hms(3661) should return "1:01:01"
-    pass
+
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+
+    return f"{hours:02d} :{minutes:02d} :{seconds:02d}"
 
 
 def admission_price(age):
-    # TODO (Part 2): return the ticket price (a number) for someone of this age
-    pass
+
+    price = 10.0
+
+    if(age < 0):
+        return "Age must be more than 0"
+
+    if(age < 5):
+        price = 0.0
+
+    elif(5 <= age <= 12):
+        price = 8.0
+
+    elif(13 <= age <= 64):
+        price = 15.0
+
+    return price
+
 
 
 def sum_multiples(limit):
-    # TODO (Part 3): return the sum of every whole number below `limit`
-    #   that is a multiple of 3 or of 5
-    pass
+
+    num = 0
+    sum = 0
+
+    while(num < limit):
+
+        if(num % 3 == 0 or num % 5 == 0):
+            sum += num
+
+        num += 1
+
+    return sum
 
 
 def total_of_positives(numbers):
-    # TODO (Part 4 - STRETCH, optional): return the sum of just the
-    #   positive numbers in the list `numbers`
-    pass
 
+    sum = 0
+
+    for num in numbers:
+        if(num >= 0):
+            sum += num
+            
+    return sum
 
 def main():
     # Optional scratch space - use this to try your functions with sample values.
@@ -37,7 +68,6 @@ def main():
     # print(sum_multiples(10))               # 23
     # print(total_of_positives([1, -2, 3]))  # 4
     pass
-
 
 if __name__ == "__main__":
     main()
